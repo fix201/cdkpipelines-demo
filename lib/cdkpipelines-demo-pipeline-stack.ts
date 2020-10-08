4636ade4faacdf00b0273702c970b9aa64a6825d
Olufisayo Joseph Ayodele <fisayoayodele01@gmail.com>
2020-10-08 13:42:26 -0500

    Initial deployment of our empty pipeline

--- lib/cdkpipelines-demo-pipeline-stack.ts
+++ lib/cdkpipelines-demo-pipeline-stack.ts
@@ -2,7 +2,6 @@ import * as codepipeline from '@aws-cdk/aws-codepipeline';
 import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions';
 import { Construct, SecretValue, Stack, StackProps } from '@aws-cdk/core';
 import { CdkPipeline, SimpleSynthAction } from "@aws-cdk/pipelines";
-import * as cdk from '@aws-cdk/core';
 
 /**
  * The stack that defines the application pipeline
@@ -22,7 +21,7 @@ export class CdkpipelinesDemoPipelineStack extends Stack {
       sourceAction: new codepipeline_actions.GitHubSourceAction({
         actionName: 'GitHub',
         output: sourceArtifact,
-        oauthToken: cdk.SecretValue.secretsManager('github-token', { jsonField : 'github-token' }),
+        oauthToken: SecretValue.secretsManager('github-token', { jsonField : 'github-token' }),
         owner: 'fix201',
         repo: 'cdkpipelines-demo',
       }),
