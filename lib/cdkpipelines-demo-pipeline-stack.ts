68d0adf84cbcc4daf7f78b2b09d792a6f79a07e1
Olufisayo Joseph Ayodele <fisayoayodele01@gmail.com>
2020-10-08 13:28:00 -0500

    Initial deployment of our empty pipeline

--- lib/cdkpipelines-demo-pipeline-stack.ts
+++ lib/cdkpipelines-demo-pipeline-stack.ts
@@ -2,6 +2,7 @@ import * as codepipeline from '@aws-cdk/aws-codepipeline';
 import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions';
 import { Construct, SecretValue, Stack, StackProps } from '@aws-cdk/core';
 import { CdkPipeline, SimpleSynthAction } from "@aws-cdk/pipelines";
+import * as cdk from '@aws-cdk/core';
 
 /**
  * The stack that defines the application pipeline
@@ -12,7 +13,6 @@ export class CdkpipelinesDemoPipelineStack extends Stack {
 
     const sourceArtifact = new codepipeline.Artifact();
     const cloudAssemblyArtifact = new codepipeline.Artifact();
- 
     const pipeline = new CdkPipeline(this, 'Pipeline', {
       // The pipeline name
       pipelineName: 'MyServicePipeline',
@@ -22,19 +22,19 @@ export class CdkpipelinesDemoPipelineStack extends Stack {
       sourceAction: new codepipeline_actions.GitHubSourceAction({
         actionName: 'GitHub',
         output: sourceArtifact,
-        oauthToken: SecretValue.secretsManager('github-token'),
+        oauthToken: cdk.SecretValue.secretsManager('github-token', { jsonField : 'github-token' }),
         owner: 'fix201',
-        repo: 'lms-backend-nodejs',
+        repo: 'cdkpipelines-demo',
       }),
 
        // How it will be built and synthesized
-       synthAction: SimpleSynthAction.standardNpmSynth({
-         sourceArtifact,
-         cloudAssemblyArtifact,
-         
-         // We need a build step to compile the TypeScript Lambda
-         buildCommand: 'npm run build'
-       }),
+      synthAction: SimpleSynthAction.standardNpmSynth({
+        sourceArtifact,
+        cloudAssemblyArtifact,
+        
+        // We need a build step to compile the TypeScript Lambda
+        buildCommand: 'npm run build'
+      }),
     });
 
     // This is where we add the application stages
